6be4a4a02960fe49d89d9517b729ffc8c439ab3d
Marcelo Henrique <henrique19.marcelo@gmail.com>
2024-10-20 00:35:21 -0300

    feat(devise): finalizing user registration

--- lib/microservice_todo_core/models/user.rb
+++ lib/microservice_todo_core/models/user.rb
@@ -1,6 +1,8 @@
 module MicroserviceTodoCore
   module Models
     class User < ApplicationRecord
+      validates :email, presence: { message: "can't be blank" },
+                        uniqueness: { case_sensitive: false, message: "has already been taken" }
     end
   end
 end
